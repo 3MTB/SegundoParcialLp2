f1f0eded4473c95ff3ec3eb0c9d1e775e65fafb8
Franly12 <deenset16@gmail.com>
2024-03-01 21:38:05 -0400

    solo estoy probando algo

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace SegundoParcialLp2
 			// see https://aka.ms/applicationconfiguration.
 			ApplicationConfiguration.Initialize();
 			Application.Run(new Form1());
+			//hola fran
 		}
 	}
 }
